50577986b13f4acecef1e2e778f2bcf82f0240ff
1102-Wu-Joe <123991105+1102-Wu-Joe@users.noreply.github.com>
2025-09-28 20:23:15 -0700

    Add Player Health UI (does work as well space takes damage))

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    public Image healthBarFill;
+    public TMP_Text healthText;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
+    {
+        healthBarFill.fillAmount = currentHealth / maxHealth;
+        healthText.text = $"{currentHealth}/{maxHealth}";
+    }
+    
+//Temporaraily is in place to test damage (*REMOVE AFTER MOBS DO DAMAGE*)
+    void Update()
+{
+    // Using the new Input System
+    if (Keyboard.current.spaceKey.wasPressedThisFrame)
+    {
+        TakeDamage(10f);
+    }
+}
+
+    
+}
+
+
