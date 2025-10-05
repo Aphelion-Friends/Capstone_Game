8f36560dc4c4df339b4552ba7d8146d30f7463f5
Malcolm Hays <mah2005atM@gmail.com>
2025-10-05 16:49:43 -0700

    Add game pause script

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using StarterAssets;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public StarterAssetsInputs input;
+    bool paused = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input.pause)
+        {
+            Debug.Log("PAUSE");
+            paused = !paused;
+            input.pause = false;
+
+            if (paused) Pause();
+            if (!paused) Resume();
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        GetComponent<Canvas>().enabled = false;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+        GetComponent<Canvas>().enabled = true;
+    }
+}
