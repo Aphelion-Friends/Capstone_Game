0249b624f9e659c0e4023ebf6f4d67db5f4f9235
TarantulaJuice <piethespy@gmail.com>
2025-11-18 00:56:03 -0800

    Method for checking task completion added
    Goes through the objective's list of task objects and determines whether they've been completed. Also added reportTask function that checks if a task within the list has been completed

--- Assets/Scripts/Objective_System/Objective.cs
+++ Assets/Scripts/Objective_System/Objective.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Objective : MonoBehaviour
 {
     [SerializeField] List<GameObject> tasks = new List<GameObject>();
-    public int numTasks;
+    private int numTasks;
     bool isComplete = false;
 
     void Start()
@@ -13,9 +13,37 @@ public class Objective : MonoBehaviour
         numTasks = tasks.Count;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int getNumTasks() {  return numTasks; }
+
+    public void checkTaskCompletion()
     {
+        bool taskIncomplete = false;
         
+        foreach (GameObject task in tasks)
+        {
+            if(task.GetComponent<Task>().isComplete == false)   //Go through lisit of task objects and see if any are incomplete
+            {
+                taskIncomplete = true;                          //If a task isn't complete, set taskIncomplete to true to prevent the Objective from being marked complete
+            }
+        }
+
+        if (taskIncomplete) //If taskComplete is false that means all tasks are marked as complete and thus the objective can be marked as completed
+        { 
+            isComplete = true;      
+        }
+    }
+
+    public void reportTask(string reportedTask)
+    {
+        foreach (GameObject task in tasks)      //Go through the list of task objects and determine if the reported task matches any in the list 
+        {
+            if (task.GetComponent<Task>().taskName == reportedTask)
+            {
+                task.GetComponent<Task>().isComplete = true;    //If it does, set that task to be complete
+            }
+        }
+
+        checkTaskCompletion();  //Check the status of all tasks afterwards to determine if the objective has been completed
+
     }
 }
