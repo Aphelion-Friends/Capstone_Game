f5136770416d449d81f437749617bad14ddc8615
TarantulaJuice <piethespy@gmail.com>
2025-11-19 17:57:17 -0800

    Main Menu Prefab added, SpiderAss task restored

--- Assets/Scripts/Objective_System/Tasks/SpiderAss.cs
+++ Assets/Scripts/Objective_System/Tasks/SpiderAss.cs
@@ -1,3 +1,25 @@
-public class SpiderAss
+public class SpiderAss : Task
 {
+    
+    ItemObject.Name taskItem = ItemObject.Name.SpiderAss;
+    public override void ItemCollected(ItemObject.Name itemName)
+    {
+        currentAmount++;
+
+        if (taskItem == itemName)
+        {
+            currentAmount++;
+            checkTaskCompletion();
+        }
+
+    }
+
+    public override void ItemDropped(ItemObject.Name itemName)
+    {
+        if (taskItem == itemName)
+        {
+            currentAmount--;
+            checkTaskCompletion();
+        }
+    }
 }
