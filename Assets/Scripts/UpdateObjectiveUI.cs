9dcb6e7a87757b91a3016ea09cd9035de92354a3
Malcolm Hays <mah2005atM@gmail.com>
2025-11-24 15:19:12 -0800

    Add script for updating objective UI

---
+++ Assets/Scripts/UpdateObjectiveUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class UpdateObjectiveUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI objectiveName;
+    [SerializeField] private TextMeshProUGUI objectiveDescription;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
