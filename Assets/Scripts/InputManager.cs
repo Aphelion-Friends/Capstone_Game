b6f9dc49a2eb4042a648172ed62eed7c03d010c7
Malcolm Hays <mah2005atM@gmail.com>
2025-12-19 16:19:55 -0800

    Add to new input system

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,6 +10,30 @@ public class InputManager : MonoBehaviour
     private PlayerInput _playerInput;
     public PlayerInput playerInput { get { return _playerInput; } }
 
+    private Vector2 _move;
+    private Vector2 _lookDirection;
+    private Vector3 _forward;
+
+    public Vector2 move { get { return _move; } }
+    public Vector2 lookDirection { get { return _lookDirection; } }
+    public Vector3 forward { get { return _forward; } }
+
+    private InputAction _moveAction;
+    private InputAction _lookAction;
+    private InputAction _fireAction;
+    private InputAction _sprintAction;
+    private InputAction _interactAction;
+    private InputAction _inventoryAction;
+    private InputAction _flashlightAction;
+
+    public InputAction moveAction { get { return _moveAction; } }
+    public InputAction lookAction { get { return _lookAction; } }
+    public InputAction fireAction { get { return _fireAction; } }
+    public InputAction sprintAction { get { return _sprintAction; } }
+    public InputAction interactAction { get { return _interactAction; } }
+    public InputAction inventoryAction { get { return _inventoryAction; } }
+    public InputAction flashlightAction { get { return _flashlightAction; } }
+
     private void Awake()
     {
         if (_instance != null)
@@ -19,5 +43,13 @@ public class InputManager : MonoBehaviour
         _instance = this;
 
         _playerInput = GetComponent<PlayerInput>();
+
+        _moveAction = _playerInput.currentActionMap.FindAction("Move");
+        _lookAction = _playerInput.currentActionMap.FindAction("Look");
+        _fireAction = _playerInput.currentActionMap.FindAction("Fire");
+        _sprintAction = _playerInput.currentActionMap.FindAction("Sprint");
+        _interactAction = _playerInput.currentActionMap.FindAction("Interact");
+        _inventoryAction = _playerInput.currentActionMap.FindAction("Inventory");
+        _flashlightAction = _playerInput.currentActionMap.FindAction("Flashlight");
     }
 }
