8678be3114983ab64d775ec303a37c2e70382d2d
1102-Wu-Joe <123991105+1102-Wu-Joe@users.noreply.github.com>
2025-12-20 01:44:52 -0800

    Added predictionplayerprefrab, finished scripts
    I created a new player prefrab for prediction base on the videos, and finished the movement and first person scripts. there are still some bugs and things to work out.

--- Assets/Scripts/Player/FirstPersonCamera.cs
+++ Assets/Scripts/Player/FirstPersonCamera.cs
@@ -1,26 +1,53 @@
 // Adapted from this video: https://www.youtube.com/watch?v=wd3mDnogxRk&list=PLF6lFlLzb6CRom_ItuhgGRTGNArFf23uw&index=1&t=296s
-
 using UnityEngine;
+using Cinemachine; //To use the cinemachine camera
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
 
 public class FirstPersonCamera : MonoBehaviour
 {
     [SerializeField] private float _lookSensitivity = 2f;
     [SerializeField] private float _maxLookAngle = 85f;
-    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private CinemachineVirtualCamera _cinemachineCamera; //Camera I'm using for fps
 
     private Vector2 _currentRotation;
     private bool _initalized;
 
-    public Vector3 forward => Quaternion.Euler(_currentRotation.x, _currentRotation.y, 0) * Vector3.forward;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Vector3 forward => transform.forward;
+    
+    void Awake()
     {
-        
+        if (_cinemachineCamera != null)
+            _cinemachineCamera.Priority = -1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init()
     {
+        _initalized = true;
+        if (_cinemachineCamera != null)
+            _cinemachineCamera.Priority = 10;
+    }
+
+    void LateUpdate()
+    {
+        if (!_initalized) return;
+
+        Vector2 mouseDelta = Vector2.zero;
+        if (Mouse.current != null)
+            mouseDelta = Mouse.current.delta.ReadValue() * _lookSensitivity;
+
+        _currentRotation.x -= mouseDelta.y;
+        _currentRotation.x = Mathf.Clamp(_currentRotation.x, -_maxLookAngle, _maxLookAngle);
+        transform.localRotation = Quaternion.Euler(_currentRotation.x, 0f, 0f);
+
+        _currentRotation.y += mouseDelta.x;
+        if (transform.parent != null)
+            transform.parent.rotation = Quaternion.Euler(0f, _currentRotation.y, 0f);
         
+        if (TryGetComponent<Rigidbody>(out var rb))
+        {
+            rb.MoveRotation(Quaternion.Euler(0f, _currentRotation.y, 0f));
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,12 +2,60 @@
 
 using UnityEngine;
 using PurrNet.Prediction;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, PlayerMovement.MoveState>
 {
+    [SerializeField] private float _moveSpeed = 7;
+    [SerializeField] private float _acceleration = 20;
+
+    [SerializeField] private FirstPersonCamera _camera;
+    [SerializeField] private PredictedRigidbody _rigidbody;
+
+    protected override void LateAwake()
+    {
+        if(isOwner)
+            _camera.Init();
+    }
+
     protected override void Simulate(MoveInput input, ref MoveState state, float delta)
     {
+        Vector3 targetVel = (transform.forward * input.moveDirection.y + transform.right * input.moveDirection.x) * _moveSpeed;
+        _rigidbody.AddForce(targetVel * _acceleration);
 
+        var horizontal = new Vector3(_rigidbody.linearVelocity.x, 0, _rigidbody.linearVelocity.z);
+        if (horizontal.magnitude > _moveSpeed)
+            _rigidbody.velocity = new Vector3(targetVel.x, _rigidbody.velocity.y, targetVel.z);
+
+        
+            var cameraForward = input.cameraForward;
+            cameraForward.y = 0;
+            if(cameraForward.sqrMagnitude > 0.0001f)
+            {
+                _rigidbody.MoveRotation((Quaternion.LookRotation(cameraForward.normalized)));
+            }
+    }
+
+    protected override void GetFinalInput(ref MoveInput input)
+    {
+        Vector2 move = Vector2.zero;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.wKey.isPressed) move.y += 1f;
+            if (Keyboard.current.sKey.isPressed) move.y -= 1f;
+            if (Keyboard.current.dKey.isPressed) move.x += 1f;
+            if (Keyboard.current.aKey.isPressed) move.x -= 1f;
+        }
+
+        input.moveDirection = Vector2.ClampMagnitude(move, 1f);
+        input.cameraForward = _camera.forward;
+    }
+
+    protected override void SanitizeInput(ref MoveInput input)
+    {
+        if(input.moveDirection.magnitude > 1)
+            input.moveDirection.Normalize();
     }
 
     public struct MoveInput : IPredictedData
