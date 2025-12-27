8d008741ea3136e00d258e609f33b615b4139114
TarantulaJuice <piethespy@gmail.com>
2025-12-27 11:37:37 -0800

    Objective Manager Added to Scene

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,8 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera mainCam;
+    private Item lookedAtItem;
+
+    [Header("Inventory Reference")]
+    public InventoryObject inventory;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI pickupPrompt;
+
+    [Header("Pickup Settings")]
+    public float pickupRange = 3f;
+    public LayerMask itemLayer;
+
     void Start()
     {
         
