396646acb1b7480959c28c5369e487f4fb97c1f6
Malcolm Hays <mah2005atM@gmail.com>
2026-01-26 11:33:52 -0800

    Add psychotron animations stuff

--- Assets/Scripts/Enemy/Psychotron.cs
+++ Assets/Scripts/Enemy/Psychotron.cs
@@ -4,4 +4,12 @@ using PurrNet.Prediction.StateMachine;
 
 public class Psychotron : GenericEnemy
 {
+    [SerializeField] private Animator animator;
+
+    protected override void AttackTransitions(ref EnemyAttackState.AttackState state)
+    {
+        base.AttackTransitions(ref state);
+
+        animator.SetTrigger("Attack");
+    }
 }
