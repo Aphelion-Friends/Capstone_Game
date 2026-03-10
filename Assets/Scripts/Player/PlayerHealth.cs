b851e7d48a2930b42b998e20d0878344c3e143df
TarantulaJuice <piethespy@gmail.com>
2026-03-09 18:33:13 -0700

    Death Now disables player gameObject

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -15,24 +15,30 @@ public class PlayerHealth : PredictedIdentity<PlayerHealth.HealthState>
         };
     }
 
-    public void ChangeHealth(float change)
+    public bool ChangeHealth(float change)
     {
         currentState.health += change;
         Debug.Log("Current HP: " + currentState.health);
 
         if (currentState.health <= 0 && !currentState.isDead)
+        {
             currentState.isDead = true;
             Die();
+        }
+
+        return currentState.isDead;
     }
 
     private void Die()
     {
-        this.GetComponent<PlayerShoot>().enabled = false;
-        this.GetComponent<PlayerMovement>().enabled = false;
-        this.GetComponent<NetworkInventory>().enabled = false;
+        //this.GetComponent<PlayerShoot>().enabled = false;
+        //this.GetComponent<PlayerMovement>().enabled = false;
+        //this.GetComponent<NetworkInventory>().enabled = false;
 
-        MeshRenderer[] visuals = this.GetComponentsInChildren<MeshRenderer>();
+        //MeshRenderer[] visuals = this.GetComponentsInChildren<MeshRenderer>();
 
+        gameObject.tag = "Dead";
+        gameObject.SetActive(false);
 
         Debug.Log("The player died!");
     }
