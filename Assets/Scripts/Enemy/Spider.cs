45db508c0fb355eeffbce2db838e7febbce3160e
1102-Wu-Joe <123991105+1102-Wu-Joe@users.noreply.github.com>
2026-03-12 00:07:03 -0700

    Add spider attack sound

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -5,11 +5,25 @@ public class Spider : GenericEnemy
 {
     [SerializeField] private NetworkAnimator _animator;
 
+    private MultiAudioSource audioSource;
+
     public override void OnDeath()
     {
         _animator.SetTrigger("Die");
         ObjectiveManager.Instance.currentState.objective.EnemyKilled("spider");
 
         base.OnDeath();
+    }
+
+    protected override void AttackTransitions(ref EnemyAttackState.AttackState state)
+    {
+        base.AttackTransitions(ref state);
+        audioSource.Play();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        audioSource = MultiAudioSource.FromResource(this.gameObject, "Spiderattack");
     }
 }
