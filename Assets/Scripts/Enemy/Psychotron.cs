e1b8b94ae6989fa1197ef826be9107e6ae9544cf
Malcolm Hays <mah2005atM@gmail.com>
2026-04-21 12:55:29 -0700

    Fix the null bug in psychotron too

--- Assets/Scripts/Enemy/Psychotron.cs
+++ Assets/Scripts/Enemy/Psychotron.cs
@@ -63,6 +63,12 @@ public class Psychotron : GenericEnemy
     {
         GameObject targetedPlayer = predictionManager.hierarchy.GetGameObject(state.targetedPlayer);
 
+        if (targetedPlayer == null)
+        {
+            state.targetedPlayer = null;
+            stateMachine.SetState(enemyPatrolState);
+        }
+
         if (Vector3.Distance(transform.position, targetedPlayer.transform.position) >= chaseRange)
         {
             stateMachine.SetState(enemyPatrolState);
