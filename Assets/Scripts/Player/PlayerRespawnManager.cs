3bc94156e0e41ac3d37ba38575fc593a294aec2b
Malcolm Hays <mah2005atM@gmail.com>
2026-04-21 13:20:22 -0700

    Add a scipt for managing player respawns

---
+++ Assets/Scripts/Player/PlayerRespawnManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using PurrNet.Prediction;
+
+public class PlayerRespawnManager : PredictedIdentity<PlayerRespawnManager.RespawnState>
+{
+    public static PlayerRespawnManager Instance;
+
+    [SerializeField] private float _respawnTimer = 5f;
+    [SerializeField] private GameObject deathScreen;
+
+    private UpdateRespawnCountdown updateRespawnCounter;
+
+    void Awake()
+    {
+        Instance = this;
+
+        updateRespawnCounter = deathScreen.GetComponent<UpdateRespawnCountdown>();
+    }
+
+    public struct RespawnState : IPredictedData<RespawnState>
+    {
+        public float respawnTimer;
+        public bool isDead;
+
+        public void Dispose() {}
+    }
+
+    protected override RespawnState GetInitialState()
+    {
+        return new RespawnState
+        {
+            respawnTimer = _respawnTimer,
+            isDead = false,
+        };
+    }
+
+    public void PlayerDied()
+    {
+        RestartTimer();
+        currentState.isDead = true;
+    }
+
+    public void RestartTimer()
+    {
+        currentState.respawnTimer = _respawnTimer;
+    }
+
+    protected override void Simulate(ref RespawnState state, float delta)
+    {
+        if (!state.isDead) return;
+
+        if(state.respawnTimer > 0)
+            state.respawnTimer -= delta;
+        else
+            state.respawnTimer = 0f;
+
+        updateRespawnCounter.SetCounterValue((int) state.respawnTimer);
+    }
+}
